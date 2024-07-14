66234ba9057d2d665e20578a41d1c330cdc76c5d
Al-Mamun <mamun@leapinglogic.com>
2024-07-14 06:35:44 +0600

    IR-9, still working...

--- drop-product-in-cart/script.js
+++ drop-product-in-cart/script.js
@@ -1,14 +1,17 @@
 
-function dropToCart(target, {
+function dropToCart(addToCartEvent, {
     productAreaSelector='.product',
     targetInsideOfProduct='img',
     cartSelector='#cart',
     animationInSecond=1.1,
-    destination_opacity=0.5,
-    destination_width=0, // in pixel >> by default auto as cart width height
-    destination_height=0, // in pixel >> by default auto as cart width height
+    target_opacity=0.5,
+    target_width=20, // in pixel
+    target_height=20, // in pixel
+    target_adjust_left=0, // in pixel
+    target_adjust_top=0, // in pixel
+    target_style='',
 }={}){
-    let addToCartBtn = target;
+    let addToCartBtn = addToCartEvent.target;
     let cart = document.querySelector(cartSelector);
     let cartBound = cart.getBoundingClientRect();
     if(cart){
@@ -16,26 +19,41 @@ function dropToCart(target, {
         let img = product.querySelector(targetInsideOfProduct);
         let bound = img.getBoundingClientRect();
         if(img){
-            let clonedImg = img.cloneNode(true);
-            console.log(clonedImg);
+            let clonedImg = img.cloneNode(true);     
+            if(target_style) clonedImg.setAttribute('style', target_style);
             clonedImg.style.width = bound.width + 'px';
             clonedImg.style.height = bound.height + 'px';
             clonedImg.style.position = 'fixed';
-            clonedImg.style.left = (bound.left + 20) + 'px';
-            clonedImg.style.top = (bound.top + 20) + 'px';
-            clonedImg.style.border = '1px solid green';
-            clonedImg.style.zIndex = '4454545454545';
+            clonedImg.style.left = bound.left + 'px';
+            clonedImg.style.top = bound.top + 'px';
+            clonedImg.style.zIndex = '100000000000';
             clonedImg.style.transition = `all ${animationInSecond}s`;
 
             document.body.appendChild(clonedImg);
 
-            // Animation start
+            // Animation startaddToCartBtn
             setTimeout(()=>{
-                clonedImg.style.left = cartBound.left + 'px';
-                clonedImg.style.top = cartBound.top + 'px';
-                clonedImg.style.opacity = destination_opacity;
-                clonedImg.style.width = (destination_width || cartBound.width) + 'px';
-                clonedImg.style.height = (destination_height || cartBound.height) + 'px';
+                
+                clonedImg.style.left = (cartBound.left + target_adjust_left) + 'px';
+
+              
+                const y = addToCartEvent.y;        
+                
+                let top_distance = bound.top - cartBound.top
+                if(top_distance > window.innerHeight){
+         
+                    console.log('calculation', bound.height - y)
+                    // clonedImg.style.top = `${bound.height - y}px`;
+                    clonedImg.style.top = `-100px`;
+                }else{
+                    clonedImg.style.top = (cartBound.top + target_adjust_top) + 'px';
+                }
+
+                clonedImg.style.opacity = target_opacity;
+                clonedImg.style.width = (target_width || cartBound.width) + 'px';
+                clonedImg.style.height = (target_height || cartBound.height) + 'px';
+
+
                 setTimeout(()=>{
                     clonedImg.remove();
                 }, (animationInSecond + 1000))
@@ -50,7 +68,12 @@ document.addEventListener('DOMContentLoaded', (e)=>{
         e.preventDefault()
 
         if(target.matches('.addToCart')){
-            dropToCart(target)
+            dropToCart(e, {
+                cartIsInFixdPosition: false,
+                target_adjust_left: 5,
+                target_adjust_top: 5,
+                target_style: 'border-radius:15px;border: 4px solid red',
+            })
         }
     })
 })

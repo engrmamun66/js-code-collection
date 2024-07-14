d50fb6bd409eaad3bf466491e657924afc98562c
Al-Mamun <mamun@leapinglogic.com>
2024-07-14 23:13:06 +0600

    IR-9, still working...

--- drop-product-in-cart/script.js
+++ drop-product-in-cart/script.js
@@ -1,7 +1,7 @@
 
 function dropToCart(add_to_cart_event, {
-    product_selector='.product',
-    target_selector='img',
+    product_selector='.product', // selector | HTMLElement
+    target_selector='img', // selector | '__SELF__'
     cart_selector='#cart',
     animation_time=1.5, // in secode
     target_opacity=0.5,
@@ -12,88 +12,144 @@ function dropToCart(add_to_cart_event, {
     target_style='',
     target_class='dropping-to-cart',
     quantity=1,
-    clone_gap=20, // For Multiple Qty
+    clone_gap=-20, // For Multiple Qty
     step_time=500, // For Multiple Qty
 }={}){
 
-    let is_success = false;
+    let response = {
+        is_success: false,
+        cartElement: null,
+        error: '',
+    };
 
     add_to_cart_event.preventDefault();
-
+    
     let addToCartBtn = add_to_cart_event.target;
-    let cart = document.querySelector(cart_selector);
-    if(cart){
-        let cartBound = cart.getBoundingClientRect();
-        let product = addToCartBtn.closest(product_selector);
-        let target = product.matches(target_selector) ? product : product.querySelector(target_selector);
-        if(target){
-            let bound = target.getBoundingClientRect();
-            Array.from({length: quantity}).fill(1).forEach((v, i)=>{
-                const adjust_position = i * clone_gap;
-                const adjust_time = +((i * step_time) / 1000).toFixed(2);
-
-                let clonedTarget = target.cloneNode(true);     
-                if(target_style) clonedTarget.setAttribute('style', target_style);
-                clonedTarget.classList.add(target_class);
-                clonedTarget.style.width = bound.width + 'px';
-                clonedTarget.style.height = bound.height + 'px';
-                clonedTarget.style.position = 'fixed';
-                clonedTarget.style.textWrape = 'nowrap';
-                clonedTarget.style.overflow = 'hidden';
-                clonedTarget.style.zIndex = '100000000000';
-                clonedTarget.style.pointerEvents = 'none';
-                clonedTarget.style.transition = `all ${animation_time + adjust_time}s`;
-                clonedTarget.style.left = bound.left + adjust_position + 'px';
-                clonedTarget.style.top = bound.top + adjust_position + 'px';
-
-                document.body.appendChild(clonedTarget);
-
-                // Animation startaddToCartBtn
-                setTimeout(()=>{
-                    
-                    clonedTarget.style.left = (cartBound.left + target_adjust_left) + 'px';
-
-                
-                    const y = add_to_cart_event.y;        
-                    
-                    let top_distance = bound.top - cartBound.top
-                    if(top_distance > window.innerHeight){         
-                        // clonedTarget.style.top = `-100px`;
-                        clonedTarget.style.top = `-100px`;
-                    }else{
-                        let cart_in_footer = cartBound.top > bound.top;
-                        if(cart_in_footer){
-                            if(cartBound.top > (window.innerHeight + 200)){
-                                clonedTarget.style.top = `${window.innerHeight + 100}px`;
-                            } else {
-                                clonedTarget.style.top = (cartBound.top + target_adjust_top) + 'px';
-                            }
-                        } else {
-                            clonedTarget.style.top = (cartBound.top + target_adjust_top) + 'px';
+    let cart = response.cart = document.querySelector(cart_selector);
+    if(add_to_cart_event instanceof Event){
+        if(cart){
+    
+            /* ----------------------------------------------- */
+            /*              With cart Animation                */
+            /* ----------------------------------------------- */
+            if(!document.querySelector('#cart_animation')){
+                let cssContents = `
+                    *.shakeCart{
+                        animation: shakeCart 0.4s ease-in-out forwards;
+                    }
+        
+                    @keyframes shakeCart {
+                        25% {
+                            transform: translateX(6px);
+                        }
+                        50% {
+                            transform: translateX(-4px);
+                        }
+                        75% {
+                            transform: translateX(2px);
+                        }
+                        100% {
+                            transform: translateX(0);
                         }
                     }
-
-                    clonedTarget.style.opacity = target_opacity;
-                    clonedTarget.style.width = (target_width || cartBound.width) + 'px';
-                    clonedTarget.style.height = (target_height || cartBound.height) + 'px';
-
-
-                    setTimeout(()=>{
-                        clonedTarget.remove();
-                        is_success = true;
-                    }, ((animation_time + adjust_time) * 1000));               
-
-                }, 0);
-            })
-            
-        }
-    }
+                `
+                let style_el = document.createElement('style')
+                style_el.id = 'cart_animation';
+                var head = document.getElementsByTagName('head')[0];
+                if (style_el.styleSheet) {   // IE
+                    style_el.styleSheet.cssText = cssContents;
+                } else {                // the world
+                    style_el.appendChild(document.createTextNode(cssContents));
+                }
+                head.appendChild(style_el);
+            } /* ----------End Cart Animation---------------- */
     
+    
+            let cartBound = cart.getBoundingClientRect();
+            let product = (product_selector instanceof HTMLElement) ? product_selector : addToCartBtn.closest(product_selector);
+            if(product){
+                let target = (product.matches(target_selector) || target_selector==='__SELF__') ? product : product.querySelector(target_selector);
+                if(target){
+                    let bound = target.getBoundingClientRect();
+                    Array.from({length: quantity}).fill(1).forEach((v, i)=>{
+                        const adjust_position = i * clone_gap;
+                        const adjust_time = +((i * step_time) / 1000).toFixed(2);
+        
+                        let clonedTarget = target.cloneNode(true);     
+                        if(target_style) clonedTarget.setAttribute('style', target_style);
+                        clonedTarget.classList.add(target_class);
+                        clonedTarget.style.width = bound.width + 'px';
+                        clonedTarget.style.height = bound.height + 'px';
+                        clonedTarget.style.position = 'fixed';
+                        clonedTarget.style.textWrape = 'nowrap';
+                        clonedTarget.style.overflow = 'hidden';
+                        clonedTarget.style.zIndex = '100000000000';
+                        clonedTarget.style.pointerEvents = 'none';
+                        clonedTarget.style.transition = `all ${animation_time + adjust_time}s`;
+                        clonedTarget.style.left = bound.left + adjust_position + 'px';
+                        clonedTarget.style.top = bound.top + adjust_position + 'px';
+        
+                        document.body.appendChild(clonedTarget);
+        
+                        // Animation startaddToCartBtn
+                        setTimeout(()=>{
+                            
+                            clonedTarget.style.left = (cartBound.left + target_adjust_left) + 'px';
+        
+                        
+                            const y = add_to_cart_event.y;        
+                            
+                            let top_distance = bound.top - cartBound.top
+                            if(top_distance > window.innerHeight){         
+                                // clonedTarget.style.top = `-100px`;
+                                clonedTarget.style.top = `-100px`;
+                            }else{
+                                let cart_in_footer = cartBound.top > bound.top;
+                                if(cart_in_footer){
+                                    if(cartBound.top > (window.innerHeight + 200)){
+                                        clonedTarget.style.top = `${window.innerHeight + 100}px`;
+                                    } else {
+                                        clonedTarget.style.top = (cartBound.top + target_adjust_top) + 'px';
+                                    }
+                                } else {
+                                    clonedTarget.style.top = (cartBound.top + target_adjust_top) + 'px';
+                                }
+                            }
+        
+                            clonedTarget.style.opacity = target_opacity;
+                            clonedTarget.style.width = (target_width || cartBound.width) + 'px';
+                            clonedTarget.style.height = (target_height || cartBound.height) + 'px';
+        
+        
+                            setTimeout(()=>{
+                                clonedTarget.remove();
+                                response.is_success = true;                        
+                            }, ((animation_time + adjust_time) * 1000));               
+        
+                        }, 0);
+                    })
+                    
+                } else {
+                    response.error = 'Target not found';
+                }
+            } else {
+                response.error = 'Product not found';
+            }
+        } else {
+            response.error = 'Cart element not found'
+        }
+    } else {
+        response.error = 'First Parameter is not a valid event object'
+    }
     let extra_time_when_qty_is_1_more = (quantity - 1) * step_time;
 
     return new Promise((resolve, reject) => {
         setTimeout(() => {    
-            resolve({success: is_success});           
+            resolve({success: is_success});   
+            cart.classList.add('shakeCart');
+            setTimeout(() => {
+                cart.classList.remove('shakeCart');
+            }, 400);        
         }, (animation_time * 1000) + 10 + extra_time_when_qty_is_1_more);
     }); 
 }
@@ -105,9 +161,8 @@ document.addEventListener('DOMContentLoaded', (e)=>{
                 target_selector: 'img',
                 target_adjust_left: 8,
                 target_adjust_top: 5,
-                quantity: 5,
-                clone_gap: 60,
-                step_time: 200,
+                quantity: 3,
+                clone_gap: -20,
             } ).then(({success}) => {
                 console.log({success});
             })
@@ -120,6 +175,8 @@ document.addEventListener('DOMContentLoaded', (e)=>{
                 cart_selector: '#cartFooter',
                 target_adjust_left: 8,
                 target_adjust_top: 5,
+                quantity: 5,
+                clone_gap: -20,
             } ).then(({success}) => {
                 console.log({success});
             })
